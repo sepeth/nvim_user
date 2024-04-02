4d75da8811b3b123fb9c6b106a6ef219bc5f79a4
Doğan Çeçen <sepeth@gmail.com>
2024-04-02 08:31:50 +0200

    Disable auto formatting for C, C++, and Markdown
    A bit of an arbitrary choice based on what I work on currently.

--- init.lua
+++ init.lua
@@ -20,6 +20,19 @@ end
 
 return {
 
+  lsp = {
+    formatting = {
+      format_on_save = {
+        enabled = true, -- enable format on save
+        ignore_filetypes = { -- disable format on save for specified filetypes
+          "markdown",
+          "cpp",
+          "c",
+        }
+      },
+    },
+  },
+
   mappings = {
 
     n = {
